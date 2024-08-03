bdfe8ffd740cbcb4685b919e2043bcad30dba1a4
zer0xxx0 <154697408+zer0xxx0@users.noreply.github.com>
2024-08-03 11:27:59 +0300

    Delete tokens_data directory

--- tokens_data/encode_image.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-with open('image/image.webp', 'rb') as f:
-    img = open('image.raw', 'wb')
-    img.write(base64.b64encode(f.read()))
-    img.close()
-
-if __name__ == '__main__':
-    pass
